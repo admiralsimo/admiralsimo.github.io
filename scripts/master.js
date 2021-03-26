f057811a27cc1c794d61c0a25b9688fbb23ab096
admiralsimo <79946006+admiralsimo@users.noreply.github.com>
2021-03-26 15:16:09 +0100

    good

--- scripts/master.js
+++ scripts/master.js
@@ -1,5 +1,5 @@
-username_data = document.getElementById("username").value;
-password_data = document.getElementById("password").value;
+var username_data = document.getElementById("username").value;
+var password_data = document.getElementById("password").value;
 function hello() {
     document.write('<p>this is your username: ' + username_data + "</p>");
     document.write('<p>this is your password: ' + password_data + "</p>");
